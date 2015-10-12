6feb40adcfa37f166302e28ee7995167f018cf0c
kennj87 <mail@kennj.com>
2015-10-12 13:34:49 +0200

    Create GameCountdown.java

---
+++ GameCountdown.java
@@ -0,0 +1,12 @@
+package endergolf.blockynights;
+
+public class GameCountdown {
+	
+	Main main;
+	Commands commands;
+	public GameCountdown(Main main, Commands commands) {
+		this.main = main;
+		this.commands = commands;
+	}
+	
+}
